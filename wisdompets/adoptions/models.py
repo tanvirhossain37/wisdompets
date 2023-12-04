c12fdf0a104490983d95f36e354eda5eb6bd0979
tanvirhossain37 <tanvir.hossain37@gmail.com>
2023-12-04 20:00:35 +0600

    Completes the websites.

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -7,10 +7,14 @@ class Pet(models.Model):
     submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
-    desciption = models.TextField()
+    description = models.TextField()
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField()
+    vaccinations = models.ManyToManyField('Vaccine', blank=True)
 
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+from django.http import Http404
+from .models import Pet
 
 # Create your views here.
+
+# def home(request):
+#     return HttpResponse('Home View!')
+
+# def pet_detail(request, pet_id):
+#     return HttpResponse(f'Pet detail with the id: {pet_id}')
+
+def home(request):
+    pets = Pet.objects.all()
+    return render(request, 'home.html', {
+        'pets': pets,
+    })
+
+def pet_detail(request, pet_id):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+    except Pet.DoesNotExist:
+        raise Http404('Pet not found')
+
+    return render(request, 'pet_detail.html', {
+        'pet': pet
+    })
